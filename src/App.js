a4834983c414791bdc0bc598bb963345d89b813c
Andrey <ustavitski.andrei@gail.com>
2023-04-09 18:36:19 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import './Index.css';
+import {useState} from "react";
 function App() {
-  return (
-    <div>
-      App
-    </div>
-  );
+    const [count, setCount] = useState(0)
+
+    const onClickPlus = () => {
+        setCount(count + 1);
+    }
+
+    const onClickMinus = () => {
+        setCount(count - 1);
+    }
+
+    return (
+        <div className="App">
+            <div>
+                <h2>Счетчик:</h2>
+                <h1>{count}</h1>
+                <button className="minus" onClick={onClickMinus}>- Минус</button>
+                <button className="plus" onClick={onClickPlus}>Плюс +</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
